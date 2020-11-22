fa63b8b8c0730e3428394ab6f93fc93ddf773f47
Juan Pablo Solano <juanpa_solano@hotmail.com>
2020-11-22 00:13:12 -0500

    better module init

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,17 @@ import Lazyload from "./lazyload";
 (function () {
   class App {
     constructor() {
+      this.initializeModules();
+      this.initializeEditor();
+    }
+    initializeModules(){
       //Lazy
       Lazyload.init();
+    }
+    initializeEditor() {
+      document.addEventListener("shopify:section:load", () => {
+        this.initializeModules();
+      });
     }
   }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./theme/**/*.liquid"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors,
     extend: {},
   },
   variants: {
